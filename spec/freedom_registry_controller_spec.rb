966176f8ef7d74f0284733784b0fdfa938b3652d
melaniehall <melaniehalldesign@gmail.com>
2013-06-04 13:11:26 -0500

    Refactor: Cleaned up freedom_registry_controller_spec

--- spec/freedom_registry_controller_spec.rb
+++ spec/freedom_registry_controller_spec.rb
@@ -8,7 +8,7 @@ describe FreedomRegistryController, ".find_by" do
   subject { FreedomRegistryController.new.find_by(selection) }
   let(:expected_message) { FreedomRegistry::NO_RESULTS }
 
-  context "selected state is empty" do
+  context "state: search-term is empty" do
     let!(:matching_organization) {
       Organization.create! do |org|
         org.name = "My Org1"
@@ -30,7 +30,7 @@ describe FreedomRegistryController, ".find_by" do
     end
   end
 
-  context "has one matching entry" do
+  context "state: search-term has one entry" do
     let!(:matching_organization) {
       Organization.create! do |org|
         org.name = "My Org1"
@@ -53,7 +53,7 @@ describe FreedomRegistryController, ".find_by" do
     end
   end
 
-   context "has no matching ies" do
+   context "state search-term: has no matching entries" do
     let!(:matching_organization) {
       Organization.create! do |org|
         org.name = "My Org1"
@@ -76,13 +76,8 @@ describe FreedomRegistryController, ".find_by" do
        expect(subject).to eql (formatted_output)
     end
   end
-end
 
-describe FreedomRegistryController, ".find_by" do
-  subject { FreedomRegistryController.new.find_by(selection) }
-  let(:expected_message) { FreedomRegistry::NO_RESULTS }
-
-  context "name value is empty" do
+  context "name: search-term is empty" do
     let!(:selection) { "by name " }
 
     it "returns an empty string by default" do
@@ -90,7 +85,7 @@ describe FreedomRegistryController, ".find_by" do
     end
   end
 
-  context "has one matching entry" do
+  context "name: search-term has one matching entry" do
     let!(:matching_organization) {
       Organization.create! do |org|
         org.name = "Org International"
@@ -106,6 +101,38 @@ describe FreedomRegistryController, ".find_by" do
       end
     }
     let!(:selection) { "by name International" }
+
+    it "returns only formatted organizations with similar names to selection" do
+      subject.should include(matching_organization.name, matching_organization.mailing_city, matching_organization.mailing_state)
+    end
+  end
+
+    context "keyword: search-term is empty" do
+    let!(:selection) { "by keyword " }
+
+    it "returns an empty string by default" do
+      expect(subject).to eql("")
+    end
+  end
+
+  context "keyword: search-term has one matching entry" do
+    let!(:matching_organization) {
+      Organization.create! do |org|
+        org.name = "Org International"
+        org.mailing_city = "City"
+        org.mailing_state = "ME"
+        org.mission_statement = "We love people."
+      end
+    }
+    let!(:ignored_organization) {
+      Organization.create! do |org|
+        org.name = "Organization Domestic"
+        org.mailing_city = "City"
+        org.mailing_state = "FL"
+        org.mission_statement = "We help people."
+      end
+    }
+    let!(:selection) { "by keyword love" }
 
     it "returns only formatted organizations with similar names to selection" do
       subject.should include(matching_organization.name, matching_organization.mailing_city, matching_organization.mailing_state)
